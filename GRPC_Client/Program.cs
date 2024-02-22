525bf6da2f6e99a08dd64b358a73b3ebf542bac5
Sanjay Solanki <1000051929@hexaware.com>
2024-02-22 07:45:16 -0500

    Initial Draft

---
+++ GRPC_Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Grpc.Core;
+
+namespace GRPC_Client
+{
+    class Program
+    {
+        const string target = "127.0.0.1:50051";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Client!");
+            try
+            {
+                Channel channel = new Channel(target, ChannelCredentials.Insecure);
+                channel.ConnectAsync().ContinueWith(t =>
+                {
+                    if (t.Status == System.Threading.Tasks.TaskStatus.RanToCompletion)
+                        Console.WriteLine("Connected!!!!");
+                });
+                var client = new Dummy.DummyService.DummyServiceClient(channel);
+                channel.ShutdownAsync().Wait();
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            { }
+        }
+    }
+}
---
+++ GRPC_Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Grpc.Core;
+
+namespace GRPC_Server
+{
+    class Program
+    {
+        const int port = 50051;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Server!");
+            Server server = null;
+            try
+            {
+                server = new Server()
+                {
+                    Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Message: " + ex.Message);
+            }
+            finally {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
+        }
+    }
+}
